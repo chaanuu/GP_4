3b07a69b0366c0578fdf9bd8626ee63eddf7e89f
jyb1018@gmail.com <jyb1018@gmail.com>
2025-11-03 12:04:34 +0900

    수정 : 맞지 않는 함수명 수정 및 필요한 라우터 추가 및 필요없는 (ExerciseLog로 이관된) 라우터 삭제

--- server/routes/Exercise.js
+++ server/routes/Exercise.js
@@ -2,12 +2,12 @@ import { ExerciseController } from "../controllers/Exercise/ExerciseController";
 import express from 'express';
 const router = express.Router();
 
-router.get('/user/:uid', ExerciseController.getAllUserExercises);
+router.get('/user/:uid', ExerciseController.getAllExercisesByUserId);
 router.post('/', ExerciseController.createExercise);
 router.delete('/:id', ExerciseController.deleteExercise);
-router.delete('/user/:userid', ExerciseController.deleteUserExercises);
-router.get('/user/:uid/date/:date', ExerciseController.getAllUserExercisesByDate);
-router.get('/user/:uid/daterange/:startDate/:endDate', ExerciseController.getAllUserExercisesByDateRange);
-
+router.put('/:id', ExerciseController.updateExercise);
+router.get('/static', ExerciseController.getAllStaticExercises);
+router.get('/code/:code', ExerciseController.getExerciseByCode);
+router.get('/:id', ExerciseController.getExerciseById);
 
 export const exerciseRouter = router;
